62b9d5291f0aec4e69001888b684c0c0ad072d8f
Ayrat Sattarov <sata@1cnw.ru>
2020-07-29 13:11:48 +0300

    Initial commit

---
+++ build.cmd
@@ -0,0 +1,11 @@
+@chcp 65001
+
+@rem формирование файла конфигурации. для включения раскомментируйте код ниже
+call vrunner compile --src src/cf --out build/1cv8.cf %*
+
+@rem собрать внешние обработчики и отчеты в каталоге build
+@rem call vrunner compileepf src/epf/МояВнешняяОбработка build %*
+@rem call vrunner compileepf src/erf/МойВнешнийОтчет build %*
+
+@rem собрать расширения конфигурации внутри ИБ
+@rem call vrunner compileext src/cfe/МоеРасширение МоеРасширение %*
